dda6928533b59803e90509d4c5fd74bb059ff78d
Дмитрий Карташов <cit.utc@gmail.com>
2015-10-25 20:42:49 +0200

    Добавлен пустой компонент для проверки коллизий

---
+++ WinnieTerminator/Core/ColliderComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace WinnieTerminator.Core
+{
+    /// <summary>
+    /// Класс, реализующий компонент, отвечающий за контроль коллизий
+    /// </summary>
+    public abstract class ColliderComponent : Component
+    {
+        /// <summary>
+        /// Активный или пассивный коллайдер
+        /// (пассивный коллайдер сам не проверяет факт коллизии. Например, ландшафт, 
+        /// он сам не перемещается и его коллизии проверять не надо. Игровые же объекты - активные.)
+        /// </summary>
+        private bool isActiveCollider = false;
+
+
+        public ColliderComponent(string id, GameObject owner) : base(id, owner)
+        {
+
+        }
+
+        public override void update(GameTime gameTime)
+        {
+
+        }
+
+    }
+}
